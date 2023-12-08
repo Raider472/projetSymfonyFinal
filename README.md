f0ae1313f121f8506f801ce46105f48f1c583f91
Raider472 <gabrielnae1@gmail.com>
2023-12-08 14:35:01 +0100

    Initialisation du projet

---
+++ README.md
@@ -0,0 +1,7 @@
+prérequis:
+    - Xamp
+    - composer
+
+installation:
+    - cloner le projet dans le fichier "htdocs" de Xamp
+    - composer install dans le repértoire root du dossier
---
+++ src/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    #[Route('/test', name: 'app_test')]
+    public function number()
+    {
+        dd('welcome! ');
+    }
+
+    #[Route('/test2', name: 'app_test2')]
+    public function number2()
+    {
+        dd('welcome! ');
+    }
+}
