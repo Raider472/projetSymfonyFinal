93e51fa100635cadc2d8ceadd53db78b7634a3b7
Raider472 <gabrielnae1@gmail.com>
2024-01-26 15:55:40 +0100

    Tableau de Bord

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\FactionRepository;
+use App\Repository\FigurineRepository;
+use App\Repository\PaintRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +15,19 @@ class AccueilController extends AbstractController
     public function accueil(): Response
     {
         return $this->render('accueil.html.twig', [
+        ]);
+    }
+
+    #[Route('/tableauDeBord', name: 'tableauDeBord')]
+    public function tableauDeBord(PaintRepository $paintRepository, FigurineRepository $figurineRepository, FactionRepository $factionRepository): Response
+    {
+        $paints = $paintRepository->findAll();
+        $figurines = $figurineRepository->findAll();
+        $factions = $factionRepository->findAll();
+        return $this->render('tableauBord.html.twig', [
+            "figurines" => $figurines,
+            "paints" => $paints,
+            "factions" => $factions,
         ]);
     }
 }
